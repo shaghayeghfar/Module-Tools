5de6bf945d6b3daa12fda0ed929a5519e242f38b
Shaghayeghfar <shadi.sh.r86@com>
2026-07-10 22:07:50 +0100

    Complete jq exercises

--- jq/script-10.sh
+++ jq/script-10.sh
@@ -5,3 +5,4 @@ set -euo pipefail
 # The input for this script is the scores.json file.
 # TODO: Write a command to output the total of adding together all players' first scores.
 # Your output should be exactly the number 54.
+jq 'map(.scores[0]) | add' scores.json
