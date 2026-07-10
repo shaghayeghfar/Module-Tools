5e06d4d6c413e065048703d2aee45a0d20d6e49e
Shaghayeghfar <shadi.sh.r86@com>
2026-07-10 23:22:11 +0100

    shell pipelines done

--- shell-pipelines/sort-uniq-head-tail/script-07.sh
+++ shell-pipelines/sort-uniq-head-tail/script-07.sh
@@ -1,8 +1,1 @@
-#!/bin/bash
-
-set -euo pipefail
-
-# The input for this script is the events-with-timestamps.txt file.
-# TODO: Write a command to show how many times anyone has entered and exited.
-# It should be clear from your script's output that there have been 5 Entry events and 4 Exit events.
-# The word "Event" should not appear in your script's output.
+cut -d' ' -f3 events-with-timestamps.txt | sort | uniq -c
